75d9294efe4ccf66091845fa4471dc9ee92c53a3
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-01-16 22:01:08 -0800

    Add operations functions - Create add, subtract, multiply and divide functions

---
+++ javascript.js
@@ -0,0 +1,27 @@
+let firstNumber;
+let secondNumber;
+let operation;
+
+
+// OPERATIONS
+function add (num1,num2) {
+return  num1 + num2
+}
+
+
+function subtract (num1,num2) {
+return num1 - num2
+}
+
+function multiply (num1,num2) {
+return num1 * num2
+}
+
+function divide (num1,num2) {
+return num1 / num2
+}
+
+console.log(add(3,3));
+console.log(subtract(3,3));
+console.log(multiply(3,3));
+console.log(divide(3,3));
